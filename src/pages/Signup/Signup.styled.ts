cca37ade9a402ad0da63849f95e5a9b3b2cbf336
zhurasique <zhuraserhii@gmail.com>
2023-04-04 20:30:49 +0300

    Sigup design

--- src/pages/Signup/Signup.styled.ts
+++ src/pages/Signup/Signup.styled.ts
@@ -1,11 +1,61 @@
 import styled from "styled-components";
 
 export const Layout = styled.div`
-  max-width: 1200px;
+  text-align: center;
+  max-width: 950px;
   margin: auto;
+  border: 2px dashed #dedcdc;
+  padding: 5px 5px 15px;
+  background-color: white;
+  
+  p {
+    margin: 0;
+    color: #4a4a4a;
+    font-size: 12px;
+    font-weight: bold;
+  }
 `;
 
-export const SignupForm = styled.div`
-  max-width: 600px;
+export const Step = styled.div`
+  text-align: left;
+  padding-top: 25px;
+  max-width: 250px;
   margin: auto;
+  height: 250px;
+  
+  p {
+    margin: 0;
+    padding-bottom: 5px;
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 0 20px;
+  position: relative;
+`;
+
+export const SecondStep = styled.div`
+  
+  div {
+    text-align: center;
+    
+    p {
+      margin: 0;
+      color: #4a4a4a;
+      font-size: 10px;
+      font-weight: bold;
+      padding-bottom: 45px;
+    }
+  }
+`;
+
+export const Block = styled.div`
+  display: block;
+`;
+
+export const Previous = styled.div`
+    position: absolute;
+    left: 40px;
 `;
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,25 +1,40 @@
-import { Layout, SignupForm } from "./Signup.styled"
-import { Button, Form, Input } from "antd";
-import { LockOutlined, UserOutlined, MailOutlined, UserAddOutlined, PhoneOutlined } from "@ant-design/icons";
-import { useState } from "react";
+import {Buttons, Layout, Step, SecondStep, Block, Previous} from "./Signup.styled"
+import { Button, Form, Input, Steps } from "antd";
+import {
+    LockOutlined,
+    UserOutlined,
+    MailOutlined,
+    UserAddOutlined,
+    PhoneOutlined,
+    ArrowRightOutlined,
+    CheckOutlined,
+    InfoCircleOutlined,
+    ArrowLeftOutlined
+} from "@ant-design/icons";
+import React, { useState } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../../constants";
+import { redirect } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
+import { ErrorDiv } from "../../components/Login/Login.styled";
 
 export const Signup = (props) => {
 
+    const [current, setCurrent] = useState(0);
     const [name, setName] = useState<string>("");
     const [surname, setSurname] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    const [confirmPassword, setConfirmPassword] = useState<string>("");
     const [phoneNumber, setPhoneNumber] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
+    const { logIn, statusCode } = UserAuth();
 
     document.title = props.title;
 
-    return (
-        <Layout>
-
-            <h4>Before we start...</h4>
-            <SignupForm>
+    const firstContent = () => {
+        return (
+            <>
+                <p>Name</p>
                 <Form.Item
                     name="name"
                     hasFeedback
@@ -28,10 +43,11 @@ export const Signup = (props) => {
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                         prefix={<UserOutlined className="site-form-item-icon" />}
-                        placeholder="Name"
                         autoComplete={"name"}
+                        placeholder={"Piotr"}
                     />
                 </Form.Item>
+                <p>Surname</p>
                 <Form.Item
                     name="surname"
                     hasFeedback
@@ -40,22 +56,11 @@ export const Signup = (props) => {
                         value={surname}
                         onChange={(e) => setSurname(e.target.value)}
                         prefix={<UserAddOutlined className="site-form-item-icon" />}
-                        placeholder="Surname"
                         autoComplete={"surname"}
+                        placeholder={"Fisz"}
                     />
                 </Form.Item>
-                <Form.Item
-                    name="email"
-                    hasFeedback
-                >
-                    <Input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        prefix={<MailOutlined className="site-form-item-icon" />}
-                        placeholder="Email"
-                        autoComplete={"email"}
-                    />
-                </Form.Item>
+                <p>Phone number</p>
                 <Form.Item
                     name="phone"
                     hasFeedback
@@ -63,11 +68,37 @@ export const Signup = (props) => {
                     <Input
                         value={phoneNumber}
                         onChange={(e) => setPhoneNumber(e.target.value)}
-                        prefix={<PhoneOutlined className="site-form-item-icon" />}
-                        placeholder="Phone"
+                        prefix={<><PhoneOutlined className="site-form-item-icon" /><span>+48</span></>}
+                        suffix={"🇵🇱"}
                         autoComplete={"phone"}
+                        placeholder={"123 456 789"}
                     />
                 </Form.Item>
+            </>
+        )
+    }
+
+    const secondContent = () => {
+        return (
+            <SecondStep>
+                <div>
+                     <p>Data which will be used for future login</p>
+                </div>
+                <p>Email</p>
+                <Form.Item
+                    name="email"
+                    hasFeedback
+                >
+                    <Input
+                        type={"email"}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        prefix={<MailOutlined className="site-form-item-icon" />}
+                        autoComplete={"email"}
+                        placeholder={"piotr.fisz@gmail.com"}
+                    />
+                </Form.Item>
+                <p>Password</p>
                 <Form.Item
                     name="password"
                 >
@@ -75,31 +106,116 @@ export const Signup = (props) => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         prefix={<LockOutlined className="site-form-item-icon" />}
-                        placeholder="Password"
                         autoComplete={"current-password"}
                     />
                 </Form.Item>
-                <Form.Item
-                    name="password"
-                >
-                    <Input.Password
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        placeholder="Confirm password"
-                    />
-                </Form.Item>
-                <Form.Item>
+            </SecondStep>
+        )
+    }
+
+    const steps = [
+        {
+            title: 'Your data',
+            content: firstContent(),
+        },
+        {
+            title: 'Login data',
+            content: secondContent(),
+        },
+    ];
+
+    const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+    const showError = () => {
+        let errorText = "Unknown error";
+        if (statusCode === 400) {
+            errorText = "Wrong credentials"
+        } else if (statusCode === 500) {
+            errorText = "Server error"
+        }
+        return (
+            <ErrorDiv>
+                <InfoCircleOutlined />
+                <p>{errorText}</p>
+            </ErrorDiv>
+        )
+    }
+
+    const signup = () => {
+        const trySignUp = async () => {
+            return axios(
+                API_BASE_URL + "/account/",
+                {
+                    method: "POST",
+                    data: {
+                        user: {
+                            username: email,
+                            password: password
+                        },
+                        account: {
+                            username: email,
+                            phoneNumber: phoneNumber,
+                            name: name,
+                            surname: surname
+                        }
+                    }
+                }
+            )
+        }
+        setLoading(true);
+        trySignUp()
+            .then(res => {
+                redirect("/");
+                logIn(email, password, true, setLoading);
+            })
+    }
+
+    return (
+        <Layout>
+            <h3>Registration</h3>
+            <p>Before we start...</p>
+            <Steps
+                labelPlacement={"vertical"}
+                current={current}
+                items={items}
+                size={"small"}
+            />
+            <Step>
+                <div>{steps[current].content}</div>
+            </Step>
+            <Buttons>
+                {current < steps.length - 1 && (
                     <Button
                         type="primary"
-                        //onClick={() => tryLogin()}
-                        loading={loading}
-                        disabled={loading}
+                        onClick={() => setCurrent(current + 1)}
                     >
-                        Log In
+                        Next <ArrowRightOutlined />
                     </Button>
-                </Form.Item>
-            </SignupForm>
+                )}
+                {current === steps.length - 1 && (
+                    <Block>
+                        <Button
+                            type="primary"
+                            onClick={() => signup()}
+                            loading={loading}
+                            disabled={loading}
+                        >
+                            Sign up <CheckOutlined />
+                        </Button>
+                        {statusCode ? showError() : <></>}
+                    </Block>
+                )}
+                {current > 0 && (
+                    <Previous>
+                        <Button
+                            shape="circle"
+                            onClick={() => setCurrent(current - 1)}
+                        >
+                            <ArrowLeftOutlined />
+                        </Button>
+                    </Previous>
+                )}
+            </Buttons>
         </Layout>
     )
 }
