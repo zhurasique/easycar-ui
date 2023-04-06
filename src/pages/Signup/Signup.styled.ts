0ad5dc242ab83ec030e0e8ab763de93c58bf8ea9
zhurasique <zhuraserhii@gmail.com>
2023-04-06 13:28:35 +0300

    Final signup page

--- src/pages/Signup/Signup.styled.ts
+++ src/pages/Signup/Signup.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Layout = styled.div`
   text-align: center;
   max-width: 950px;
-  margin: 60px auto;
+  margin: 60px auto 10px auto;
   border: 2px dashed #dedcdc;
   padding: 5px 5px 15px;
   background-color: white;
@@ -62,4 +62,42 @@ export const Block = styled.div`
 export const Previous = styled.div`
     position: absolute;
     left: 40px;
+`;
+
+export const OAuth2Login = styled.div`
+  max-width: 500px;
+  margin: auto;
+  text-align: center;
+  
+  div {
+    margin-bottom: 5px;
+  }
+  
+  h4 {
+    margin-bottom: 5px;
+  }
+
+  svg {
+    padding-top: 10px;
+    max-width: 30px;
+    max-height: 30px;
+  }
+
+  p {
+    padding-left: 7px;
+    padding-top: 3px;
+    margin: 0;
+  }
+`;
+
+export const InputWithTip = styled.div`
+  padding: 0 !important;
+  position: relative;
+`;
+
+export const TipContainer = styled.div`
+  padding: 0 !important;
+  position: absolute;
+  top: 8px;
+  right: -24px;
 `;
